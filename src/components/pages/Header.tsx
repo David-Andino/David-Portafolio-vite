01de883071154f2e0b4707c683c94cb5a74e369f
Rijo Sebastian <rijobdk@gmail.com>
2024-08-25 22:32:40 +0530

    Add experice section

--- src/components/pages/Header.tsx
+++ src/components/pages/Header.tsx
@@ -9,7 +9,7 @@ interface GradientTextProps {
 
 const WordAnimation: React.FC = () => (
   <GradientText
-    className="font-display text-center text-2xl font-bold tracking-[-0.05em] sm:text-3xl md:text-4xl lg:text-5xl p-2"
+    className="font-display text-center   font-bold tracking-[-0.05em] text-3xl md:text-4xl lg:text-5xl p-2"
     text="Hi, I'm Rijo Sebastian"
   />
 );
--- src/components/pages/Portfolio.tsx
+++ src/components/pages/Portfolio.tsx
@@ -2,14 +2,16 @@ import { DockDemo } from "../ui/DockDemo";
 import ParticlesDemo from "../ui/Particles";
 import Header from "./Header";
 import About from "./About"
+import Experience from "./Experience";
 function Portfolio() {
   return (
     <div className="min-h-screen">
-      {/* <ParticlesDemo /> */}
+      <ParticlesDemo />
       <DockDemo />
       <div className="flex flex-col items-center justify-center p-2">
         <Header />
         <About />
+        <Experience />
       </div>
     </div>
   );
